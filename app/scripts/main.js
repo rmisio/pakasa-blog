61c5139d85344a9b453a3b2521d82636601f9d53
rmisio <rmisio@users.noreply.github.com>
2015-01-03 14:31:50 -0800

    building out blog post page + affixing main nav on scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+(function($) {
+    var $mainNavbar = $('header .navbar'),
+        scrollPos = $(document).scrollTop();
+
+    // Affix main navbar when scrolling down
+    $(window).scroll(
+        _.throttle(function() {
+            prevScrollPos = scrollPos;
+            scrollPos = $(document).scrollTop();
+
+            if (scrollPos > 150) {
+                // if scrolling up else scrolling down
+                if (scrollPos < prevScrollPos) {
+                    $mainNavbar.addClass('affixed');
+                } else {
+                    $mainNavbar.removeClass('affixed');
+                }
+            }
+
+        }, 250)
+    );
+
+})(jQuery);
