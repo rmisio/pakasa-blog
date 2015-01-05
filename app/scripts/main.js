f78c8e033dd55dd2448e0ffc092b28d8f4d1b723
rmisio <rmisio@users.noreply.github.com>
2015-01-05 14:13:09 -0800

    adding dummy blog posts + defer-image functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,4 +25,47 @@
         }, 100)
     );
 
+    // defer image functionality
+    $(function() {
+        $('.defer-image').each(function(i, el) {
+            console.log('boober');
+            var $el = $(this),
+                img = new Image(),
+                $placeholder = $el.find(':first-child'),
+                imgH = $el.data('imgH'),
+                imgW = $el.data('imgW');
+
+            if (!(imgH && imgW)) {
+                throw new Error('Please provide the image dimensions as data-img-h and data-img-w attributes on the .defer-image element.');
+            }
+
+            $el.css({
+                maxHeight: imgH,
+                maxWidth: imgW
+            });
+            $placeholder.css('padding-top', (imgH / imgW * 100) + '%');
+            $el.addClass('loading');
+
+            img.onload = function () {
+                $(img).css({
+                    maxHeight: imgH,
+                    maxWidth: imgW
+                });
+                $el.css({
+                    maxHeight: '',
+                    maxWidth: ''
+                });
+                $el.toggleClass('loading loaded');
+                $placeholder.replaceWith(img);
+            }
+
+            for (var i = 0; i < $placeholder[0].attributes.length; i++) {
+                var attr = $placeholder[0].attributes[i];
+
+                if (attr.name.match(/^data-/)) {
+                    img.setAttribute(attr.name.replace('data-', ''), attr.value);
+                }
+            }
+        });
+    });
 })(jQuery);
