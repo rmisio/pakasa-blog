4e5885aa23047b635bb7ba7dc0d3d20f7cc65e40
rmisio <rmisio@users.noreply.github.com>
2015-02-09 17:22:51 -0800

    affixing scrollbar by default

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,4 @@
 (function($) {
-    var $mainNavbar = $('header .navbar'),
-        mainNavbar = $mainNavbar.outerHeight(true),
-        scrollPos = $(document).scrollTop(),
-        $body = $('body');
-
-    // Affix main navbar when scrolling down
-    $(window).scroll(
-        _.throttle(function() {
-            prevScrollPos = scrollPos;
-            scrollPos = $(document).scrollTop();
-
-            if (scrollPos > 150) {
-                // if scrolling up else scrolling down
-                if (scrollPos < prevScrollPos) {
-                    $mainNavbar.addClass('affixed');
-                } else {
-                    $body.css('padding-top', '');
-                    $mainNavbar.removeClass('affixed');
-                }
-            } else if (scrollPos === 0) {
-                $mainNavbar.removeClass('affixed');
-            }
-
-        }, 100)
-    );
-
     // defer image functionality
     $('.defer-image').each(function(i, el) {
         var $el = $(this),
